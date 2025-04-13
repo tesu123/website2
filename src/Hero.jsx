a69246cbe0dbb1cab4a78b0de916dc4459eee94e
Abhijit-Rabidas <abhijit.rabidas.mca@gmail.com>
2025-04-13 15:29:17 +0530

    first commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -98,7 +98,7 @@ function Hero() {
                 Abhijit Rabidas
               </span> */}
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-emerald-400 to-blue-500">
-                Abhijit Rabidas
+                Abhijit Rabida
               </span>
             </motion.h1>
 
