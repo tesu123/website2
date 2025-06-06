d1237c6c957c6d037d21a03ed51e501466e40087
Abhijit-Rabidas <abhijit.rabidas.mca@gmail.com>
2025-06-07 01:08:42 +0530

    update4

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
-import { registerUser, verifyOTP, resendOTP, loginUser, logoutUser , getCurrentUser } from "../controllers/user.controller.js" ;
+import {
+  registerUser,
+  verifyOTP,
+  resendOTP,
+  loginUser,
+  logoutUser,
+  getCurrentUser,
+} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-const router= Router();
+const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/verify-otp").post(verifyOTP);
 router.route("/resend-otp").post(resendOTP);
 router.route("/login").post(loginUser);
-router.route("/logout").post(verifyJWT,  logoutUser);
-router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 
+hiiii;
 export default router;
